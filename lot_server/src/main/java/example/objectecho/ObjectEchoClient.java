ea7bc0ef43ccc21b9031bc0f7874bb0de491902e
haosonglin <1124106217@qq.com>
2018-04-08 18:04:30 +0800

    修改导入文件

--- lot_server/src/main/java/example/objectecho/ObjectEchoClient.java
+++ lot_server/src/main/java/example/objectecho/ObjectEchoClient.java
@@ -15,6 +15,7 @@
  */
 package example.objectecho;
 
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
@@ -22,7 +23,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.example.echo.EchoClient;
+import example.echo.EchoClient;
 import io.netty.handler.codec.serialization.ClassResolvers;
 import io.netty.handler.codec.serialization.ObjectDecoder;
 import io.netty.handler.codec.serialization.ObjectEncoder;
